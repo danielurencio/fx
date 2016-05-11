1f4ad3dd0e936e54577d03e8948f0b73600419e5
Daniel <danielurencio@gmial.com>
2016-05-11 14:04:15 -0500

    script enhanced for all years

--- script.sh
+++ script.sh
@@ -1,15 +1,19 @@
 MONTHS=(JANUARY FEBRUARY MARCH APRIL MAY JUNE JULY AUGUST SEPTEMBER OCTOBER NOVEMBER DECEMBER)
+YEARS=(2009 2010 2011 2012 2013 2014 2015 2016)
 c=0
 
-for i in ${!MONTHS[@]}
-    do a=`expr ${i} + 1`; 
-    if [ `expr length ${a}` = 1 ]; then
-	make PARITY=GBPUSD MONTH1=${c}${a} MONTH2=${MONTHS[$i]} YEAR=2010
-	make mongoimport PARITY=GBPUSD
-	make clean
-    else 
-	make PARITY=GBPUSD MONTH1=${a} MONTH2=${MONTHS[$i]} YEAR=2010
-        make mongoimport PARITY=GBPUSD
-        make clean
-    fi
+for e in ${YEARS[@]}
+    do
+	for i in ${!MONTHS[@]}
+	    do a=`expr ${i} + 1`; 
+	    if [ `expr length ${a}` = 1 ]; then
+		make PARITY=GBPUSD MONTH1=${c}${a} MONTH2=${MONTHS[$i]} YEAR=$e
+		make mongoimport PARITY=GBPUSD
+		make clean
+	    else 
+		make PARITY=GBPUSD MONTH1=${a} MONTH2=${MONTHS[$i]} YEAR=$e
+		make mongoimport PARITY=GBPUSD
+		make clean
+	    fi
+	done
 done
