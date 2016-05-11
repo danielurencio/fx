42b9138f903ede58f1e321c4b05d86b50949a14b
danielurencio <danielurencio@gmail.com>
2016-05-11 09:09:53 -0500

    midified script

--- script.sh
+++ script.sh
@@ -1,7 +1,15 @@
 MONTHS=(JANUARY FEBRUARY MARCH APRIL MAY JUNE JULY AUGUST SEPTEMBER OCTOBER NOVEMBER DECEMBER)
+c=0
 
 for i in ${!MONTHS[@]}
-    do make PARITY=GBPUSD MONTH1=`expr $i + 1` MONTH2=${MONTHS[$i]} YEAR=2010 #echo `expr $i + 1` ${MONTHS[$i]}
-       make mongoimport PARITY=GBPUSD
-       make clean
+    do a=`expr ${i} + 1`; 
+    if [ `expr length ${a}` = 1 ]; then
+	make PARITY=GBPUSD MONTH1=${c}${a} MONTH2=${MONTHS[$i]} YEAR=2010
+	make mongoimport PARITY=GBPUSD
+	make clean
+    else 
+	make PARITY=GBPUSD MONTH1=${a} MONTH2=${MONTHS[$i]} YEAR=2010
+        make mongoimport PARITY=GBPUSD
+        make clean
+    fi
 done
