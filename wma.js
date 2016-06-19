cdab5cc6971449fc8f7c4d1d45438c0fcee706cf
Daniel <danielurencio@gmial.com>
2016-06-18 21:03:43 -0500

    modified wma.js file and added the correct function for moving averages

--- wma.js
+++ wma.js
@@ -3,7 +3,7 @@ var l = db.eurusd_h.find().count();
 
 //for(var i = 0; i<l; i++) {
 var a = db.eurusd_h.aggregate([
-{ $sort: { year:-1, month:-1, day:-1 } },
+{ $sort: { year:-1, month:-1, day:-1, hour:-1 } },
 { $skip:i },
 { $limit: 8 }
 ]).toArray()
@@ -22,3 +22,19 @@ db.eurusd_h.update({ _id:a[0]._id },{ $set: { wma: result } });
 //}
 
 //}
+
+function ma(n) {
+  for(var i=0; i<docss.length; i++) {
+    var ma = [];
+//    ma.push(docs[i].bid,docs[i+1].bid,docs[i+2].bid,docs[i+3].bid,docs[i+4].bid);
+    for(var j=0; j<n; j++) {
+	ma.push( docss[i+j].bid );
+    };
+
+    var result = ma.reduce(function sum(a,b) { return a + b; }, 0);
+    result = result/n;
+
+    var name = "ma" + n;
+    docss[i][name] = result;
+  }
+}
