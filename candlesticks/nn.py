bcf3dd28314157c3d2ee3f549f0cacec021a6389
Daniel <danielurencio@gmial.com>
2017-08-26 15:18:07 -0500

    a

--- candlesticks/nn.py
+++ candlesticks/nn.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from scipy import stats
+from sklearn import preprocessing
 
 class NN(object):
   def __init__(self,file_):
     self.data = pd.read_csv(file_)
+    self.data.Date_Time = pd.to_datetime(self.data.Date_Time)
+
+  def scale(self):
+    std_scale = preprocessing.StandardScaler().fit(self.X)
+    self.X = std_scale.transform(self.X)
 
   def create_dataset(self,x,y,t):
     dSize = self.data.shape[0]
@@ -22,7 +31,7 @@ class NN(object):
       l_ = l[i:(i+x),0].tolist()
       h_ = h[i:(i+x),0].tolist()
       last = c[i:(i+x),0]
-      a_ = o_ + c_ + l_ + h_
+      a_ = c_#o_ + c_ + l_ + h_
       lows = l[(i+x):(i+x)+y,0]
       highs = h[(i+x):(i+x)+y,0]
       y_ = [ np.min(lows), np.max(highs) ]
@@ -38,8 +47,21 @@ class NN(object):
 	y_ = [0,1,0]
       dataY.append(y_)
       dataX.append(a_)
-    return np.array(dataX), np.array(dataY)
+    self.X = np.array(dataX)
+    self.Y = np.array(dataY)
+
+  def network(self):
+    self.model = Sequential()
+    self.model.add(Dense(80, input_dim = self.X.shape[1], activation='relu'))
+    self.model.add(Dense(20, activation='relu'))
+    self.model.add(Dense(20, activation='relu'))
+    self.model.add(Dense(3,activation='softmax'))
+    self.model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
 
 
 if(__name__ == "__main__"):
   nn = NN("EURUSD15Min.csv")
+  nn.create_dataset(24*4,6*4,0.0015)
+  nn.scale()
+  nn.network()
+  #nn.model.fit(nn.X, nn.Y, epochs=150, batch_size=10)
