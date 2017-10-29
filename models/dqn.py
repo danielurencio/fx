91d198b4696b5357d85b4fc5f9ab13835702c396
danielurencio <danielurencio@gmail.com>
2017-10-29 11:44:34 -0600

    d

--- models/dqn.py
+++ models/dqn.py
@@ -3,37 +3,64 @@ import numpy as np
 
 class MarketEnv:
   def __init__(self,token,dates):
-    self.lookback = 4
+    self.lookback = 9
     self.token = token
     self.dates = dates
     self.data = self.get_data()
     self.count = 1
     self.balance = 100
     self.units = 100
+    self.trade = []
 
   def get_data(self):
     data = get_candles(self.dates,self.token)
-    candles = np.array(map(lambda x:[x['openAsk'],x['highAsk'],x['closeAsk'],x['lowAsk']],data))
+    fn = lambda x:[x['openAsk'],x['highAsk'],x['closeAsk'],x['lowAsk']]
+    candles = np.array(map(fn,data))
     return self.series(candles)
 
   def series(self,data):
     series = []
-    for i in range(len(data) - (self.lookback-1) ):
+    for i in range( len(data) - (self.lookback-1) ):
       series.append(np.hstack(data[i:i+self.lookback]))
     return np.array(series)
 
   def reset(self):
     self.count = 1
-    return self.data[0]
+    return np.append(self.data[0],0)
 
-  def step(self,action=None):
-    if(self.count < len(self.data)):
-      state = self.data[self.count]
-      reward = 0
+  def step(self,action):
+    if( self.count < len(self.data) ):
+      state = np.append(self.data[self.count],len(self.trade))
+      reward = self.reward_signal(state,action)
       done = False
       self.count += 1
     else:
-      state = self.data[self.data.shape[0]]
-      reward = 0
+      state = self.data[self.data.shape[0]-1]
+      reward = self.reward_signal(state,action)
       done = True
     return state,reward,done
+
+  def reward_signal(self,state,action):
+    close_price = state[state.shape[0]-3]
+    if( len(self.trade) == 0 ):
+      if( action == 0 ):
+        self.trade.append({ "type":"sell","price":close_price })
+      if( action == 2 ):
+        self.trade.append({ "type":"buy","price":close_price })
+      reward = 0
+    else:
+      if( action == 0 and self.trade[0]['type'] == 'buy'):
+	reward = (self.trade[0]["price"] - close_price) * self.units
+	del self.trade[0]
+      if( len(self.trade) == 1 ):
+        if( action == 0 and self.trade[0]['type'] == 'sell'):
+	  reward = 0
+      if( action == 1 ):
+	reward = 0
+      if( action == 2 and self.trade[0]['type'] == 'sell'):
+	reward = (close_price - self.trade[0]["price"]) * self.units
+	del self.trade[0]
+      if( len(self.trade) == 1 ):
+        if( action == 2 and self.trade[0]['type'] == 'buy'):
+	  reward = 0
+    return reward
--- test.py
+++
@@ -1,3 +0,0 @@
-from models.dqn import MarketEnv
-
-env = MarketEnv("59f5159dcfb7d742ff3d53ddc0097be9-e760cea159c68dde7e7d22258eb9dcb5",('2017-01-02','2017-02-27'))
